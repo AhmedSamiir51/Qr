3cb64568a6fee59610916acc400fdc734e8a7269
AhmedSamiir51 <88041263+AhmedSamiir51@users.noreply.github.com>
2021-10-30 18:03:41 +0200

    first commit

---
+++ Qr/Controllers/UserController.cs
@@ -0,0 +1,89 @@
+﻿using Qr.Models;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Qr.Controllers
+{
+    public class UserController : Controller
+    {
+        private QrCode2Entities db = new QrCode2Entities();
+
+        // GET: users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user user = db.users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            var QRCodeText = new Uri(Request.Url.AbsoluteUri);
+            user.QRCodeText = QRCodeText.ToString();
+            db.SaveChanges();
+
+
+            QRCodeGenerator QrGenerator = new QRCodeGenerator();
+            QRCodeData QrCodeInfo = QrGenerator.CreateQrCode(user.QRCodeText, QRCodeGenerator.ECCLevel.Q);
+            QRCode QrCode = new QRCode(QrCodeInfo);
+            Bitmap QrBitmap = QrCode.GetGraphic(60);
+            byte[] BitmapArray = QrBitmap.BitmapToByteArray();
+            string QrUri = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(BitmapArray));
+            ViewBag.QrCodeUri = QrUri;
+
+
+            return View(user);
+        }
+
+
+        // GET: users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Date,Number")] user user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Details", new { id = user.Id });
+            }
+
+            return View(user);
+        }
+    }
+
+
+
+    public static class BitmapExtension
+    {
+        public static byte[] BitmapToByteArray(this Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+    }
+
+
+}
